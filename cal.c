ab5be42a446b9debcbd1cd634df912e2e68972f0
Sachin.B.Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-05 19:55:01 +0530

    first commit

---
+++ cal.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int get_1st_weekday(int year)
+{
+
+    int d;
+    d = (((year - 1) * 365) + ((year - 1) / 4) - ((year - 1) / 100) + ((year) / 400) + 1) % 7;
+    return d;
+}
+
+int main()
+{
+    system("cls");
+    system("Color 0A");
+    int year, month, day, daysInMonth, weekDay = 0, startingDay;
+    printf("\nEnter Year:");
+    scanf("%d", &year);
+
+    char *months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    int monthDay[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        monthDay[1] = 29;
+
+    startingDay = get_1st_weekday(year);
+
+    for (month = 0; month < 12; month++)
+    {
+
+        daysInMonth = monthDay[month];
+        printf("\n\n---------------%s-------------------\n", months[month]);
+        printf("\n  Sun  Mon  Tue  Wed  Thurs  Fri  Sat\n");
+
+        for (weekDay = 0; weekDay < startingDay; weekDay++)
+            printf("     ");
+
+        for (day = 1; day <= daysInMonth; day++)
+        {
+            printf("%5d", day);
+
+            if (++weekDay > 6)
+            {
+                printf("\n");
+                weekDay = 0;
+            }
+            startingDay = weekDay;
+        }
+    }
+}
